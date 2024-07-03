d87e938987f3a0d05d3cf8320dad613aad68a7ad
senseiGai <swerss564@gmail.com>
2024-07-03 15:44:46 +0500

    LOGO REFACTORED

--- spark-ui/src/components/Logo/index.stories.tsx
+++ spark-ui/src/components/Logo/index.stories.tsx
@@ -15,14 +15,28 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+const responsiveSettings = {
+  xxs: { width: "20px", height: "20px" },
+  xs: { width: "30px", height: "30px" },
+  s: { width: "40px", height: "40px" },
+  m: { width: "50px", height: "50px" },
+  sm: { width: "60px", height: "60px" },
+  l: { width: "70px", height: "70px" },
+  md: { width: "80px", height: "80px" },
+  tablet: { width: "90px", height: "90px" },
+  tabletSm: { width: "100px", height: "100px" },
+  lg: { width: "110px", height: "110px" },
+  xl: { width: "120px", height: "120px" },
+  twoXl: { width: "130px", height: "130px" },
+  threeXl: { width: "140px", height: "140px" },
+  fourXl: { width: "150px", height: "150px" },
+  fiveXl: { width: "160px", height: "160px" },
+  sixXl: { width: "170px", height: "170px" },
+};
+
 export const Test: Story = {
   args: {
     logo: logo,
-    xxsWidth: "20px",
-    xxsHeight: "20px",
-    xsWidth: "20px",
-    xsHeight: "20px",
-    threeXlWidth: "20px",
-    fourXlWidth: "20px",
+    responsive: responsiveSettings,
   },
 };
--- spark-ui/src/components/Logo/index.tsx
+++ spark-ui/src/components/Logo/index.tsx
@@ -1,189 +1,40 @@
 import React from "react";
 import { getBreakpoint } from "@/utils/getBreakpoint";
 import useWindowSize from "../useWindowSize";
+import { DeviceSize } from "@/types/deviceSize";
+
+interface ResponsiveProperties {
+  width?: string;
+  height?: string;
+}
 
 interface ILogoProps {
+  responsive: Record<DeviceSize, ResponsiveProperties>;
   logo: string;
   width?: string;
   height?: string;
-  xxsWidth?: string;
-  xsWidth?: string;
-  sWidth?: string;
-  mWidth?: string;
-  smWidth?: string;
-  lWidth?: string;
-  mdWidth?: string;
-  tabletWidth?: string;
-  tabletSmWidth?: string;
-  lgWidth?: string;
-  xlWidth?: string;
-  twoXlWidth?: string;
-  threeXlWidth?: string;
-  fourXlWidth?: string;
-  fiveXlWidth?: string;
-  sixXlWidth?: string;
-  xxsHeight?: string;
-  xsHeight?: string;
-  sHeight?: string;
-  mHeight?: string;
-  smHeight?: string;
-  lHeight?: string;
-  mdHeight?: string;
-  tabletHeight?: string;
-  tabletSmHeight?: string;
-  lgHeight?: string;
-  xlHeight?: string;
-  twoXlHeight?: string;
-  threeXlHeight?: string;
-  fourXlHeight?: string;
-  fiveXlHeight?: string;
-  sixXlHeight?: string;
 }
 
-export const Logo: React.FC<ILogoProps> = ({
-  logo,
-  width,
-  height,
-  xxsWidth,
-  xsWidth,
-  sWidth,
-  mWidth,
-  smWidth,
-  lWidth,
-  mdWidth,
-  tabletWidth,
-  tabletSmWidth,
-  lgWidth,
-  xlWidth,
-  twoXlWidth,
-  threeXlWidth,
-  fourXlWidth,
-  fiveXlWidth,
-  sixXlWidth,
-  xxsHeight,
-  xsHeight,
-  sHeight,
-  mHeight,
-  smHeight,
-  lHeight,
-  mdHeight,
-  tabletHeight,
-  tabletSmHeight,
-  lgHeight,
-  xlHeight,
-  twoXlHeight,
-  threeXlHeight,
-  fourXlHeight,
-  fiveXlHeight,
-  sixXlHeight,
-}) => {
+export const Logo: React.FC<ILogoProps> = ({ logo, responsive }) => {
   const { width: windowWidth } = useWindowSize();
 
-  const getResponsiveValue = (
-    xxsValue: string | undefined,
-    xsValue: string | undefined,
-    sValue: string | undefined,
-    mValue: string | undefined,
-    smValue: string | undefined,
-    lValue: string | undefined,
-    mdValue: string | undefined,
-    tabletValue: string | undefined,
-    tabletSmValue: string | undefined,
-    lgValue: string | undefined,
-    xlValue: string | undefined,
-    twoXlValue: string | undefined,
-    threeXlValue: string | undefined,
-    fourXlValue: string | undefined,
-    fiveXlValue: string | undefined,
-    sixXlValue: string | undefined,
-    defaultValue: string | undefined
-  ) => {
-    const breakpoint = getBreakpoint(windowWidth);
-    switch (breakpoint) {
-      case "xxs":
-        return xxsValue || defaultValue;
-      case "xs":
-        return xsValue || defaultValue;
-      case "s":
-        return sValue || defaultValue;
-      case "m":
-        return mValue || defaultValue;
-      case "sm":
-        return smValue || defaultValue;
-      case "l":
-        return lValue || defaultValue;
-      case "md":
-        return mdValue || defaultValue;
-      case "tablet":
-        return tabletValue || defaultValue;
-      case "tablet_sm":
-        return tabletSmValue || defaultValue;
-      case "lg":
-        return lgValue || defaultValue;
-      case "xl":
-        return xlValue || defaultValue;
-      case "2xl":
-        return twoXlValue || defaultValue;
-      case "3xl":
-        return threeXlValue || defaultValue;
-      case "4xl":
-        return fourXlValue || defaultValue;
-      case "5xl":
-        return fiveXlValue || defaultValue;
-      case "6xl":
-        return sixXlValue || defaultValue;
-      default:
-        return defaultValue;
-    }
+  const getResponsiveProperty = (
+    property: keyof ResponsiveProperties,
+    defaultValue: string
+  ): string => {
+    const breakpoint: DeviceSize = getBreakpoint(windowWidth) as DeviceSize;
+    return responsive[breakpoint]?.[property] || defaultValue;
   };
 
-  const getWidth = () =>
-    getResponsiveValue(
-      xxsWidth,
-      xsWidth,
-      sWidth,
-      mWidth,
-      smWidth,
-      lWidth,
-      mdWidth,
-      tabletWidth,
-      tabletSmWidth,
-      lgWidth,
-      xlWidth,
-      twoXlWidth,
-      threeXlWidth,
-      fourXlWidth,
-      fiveXlWidth,
-      sixXlWidth,
-      width
-    );
-
-  const getHeight = () =>
-    getResponsiveValue(
-      xxsHeight,
-      xsHeight,
-      sHeight,
-      mHeight,
-      smHeight,
-      lHeight,
-      mdHeight,
-      tabletHeight,
-      tabletSmHeight,
-      lgHeight,
-      xlHeight,
-      twoXlHeight,
-      threeXlHeight,
-      fourXlHeight,
-      fiveXlHeight,
-      sixXlHeight,
-      height
-    );
   return (
     <a href="/">
       <img
         src={logo}
         alt="Logo"
-        style={{ width: getWidth(), height: getHeight() }}
+        style={{
+          width: getResponsiveProperty("width", "35px"),
+          height: getResponsiveProperty("height", "35px"),
+        }}
       />
     </a>
   );
