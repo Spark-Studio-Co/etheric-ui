383d1946944381598dcc95982249942d03ab5ecd
senseiGai <swerss564@gmail.com>
2024-07-03 16:06:28 +0500

    SEPARATOR REFACTORED

--- spark-ui/src/components/Separator_Components/index.stories.tsx
+++ spark-ui/src/components/Separator_Components/index.stories.tsx
@@ -16,28 +16,28 @@ type Story = StoryObj<typeof meta>;
 
 export const Horizontal_Separator: Story = {};
 
+const responsiveSettings = {
+  xxs: { width: "100%", height: "1px", margin: "10px 0" },
+  xs: { width: "100%", height: "1.5px", margin: "15px 0" },
+  s: { width: "100%", height: "2px", margin: "20px 0" },
+  m: { width: "100%", height: "2.5px", margin: "25px 0" },
+  sm: { width: "100%", height: "3px", margin: "30px 0" },
+  l: { width: "100%", height: "3.5px", margin: "35px 0" },
+  md: { width: "100%", height: "4px", margin: "40px 0" },
+  tablet: { width: "100%", height: "4.5px", margin: "45px 0" },
+  tabletSm: { width: "100%", height: "5px", margin: "50px 0" },
+  lg: { width: "100px", height: "5.5px", margin: "55px 0" },
+  xl: { width: "100px", height: "6px", margin: "60px 0" },
+  twoXl: { width: "100px", height: "6.5px", margin: "65px 0" },
+  threeXl: { width: "100px", height: "7px", margin: "70px 0" },
+  fourXl: { width: "100px", height: "7.5px", margin: "75px 0" },
+  fiveXl: { width: "100px", height: "8px", margin: "80px 0" },
+  sixXl: { width: "100px", height: "8.5px", margin: "85px 0" },
+};
+
 export const Test: Story = {
   args: {
-    width: "100px",
-    height: "2px", // Default height
+    responsive: responsiveSettings,
     background: "red",
-    margin: "20px 0",
-    xxsMargin: "10px 0",
-    xsMargin: "15px 0",
-    smMargin: "20px 0",
-    mdMargin: "25px 0",
-    lgMargin: "30px 0",
-    xlMargin: "35px 0",
-    xxsHeight: "1px",  // Smallest screens
-    xsHeight: "1.5px", // Slightly larger screens
-    smHeight: "2px",   // Small devices
-    mdHeight: "2.5px", // Medium devices
-    lgHeight: "3px",   // Large devices
-    xlHeight: "3.5px", // Extra-large devices
-    twoXlHeight: "4px", // 2X large devices
-    threeXlHeight: "4.5px", // 3X large devices
-    fourXlHeight: "5px", // 4X large devices
-    fiveXlHeight: "5.5px", // 5X large devices
-    sixXlHeight: "6px" // 6X large devices
   },
 };
--- spark-ui/src/components/Separator_Components/index.tsx
+++ spark-ui/src/components/Separator_Components/index.tsx
@@ -1,238 +1,38 @@
 import React from "react";
 import { getBreakpoint } from "@/utils/getBreakpoint";
 import useWindowSize from "../useWindowSize";
+import { DeviceSize } from "@/types/deviceSize";
+
+interface ResponsiveProperties {
+  width?: string;
+  height?: string;
+  margin?: string;
+}
 
 interface IHorizontalSeparatorProps {
-  width: string;
-  height: string;
   background?: string;
-  xxsMargin?: string;
-  xsMargin?: string;
-  sMargin?: string;
-  mMargin?: string;
-  smMargin?: string;
-  lMargin?: string;
-  mdMargin?: string;
-  tabletMargin?: string;
-  tabletSmMargin?: string;
-  lgMargin?: string;
-  xlMargin?: string;
-  twoXlMargin?: string;
-  threeXlMargin?: string;
-  fourXlMargin?: string;
-  fiveXlMargin?: string;
-  sixXlMargin?: string;
-  margin?: string;
-  xxsWidth?: string;
-  xsWidth?: string;
-  sWidth?: string;
-  mWidth?: string;
-  smWidth?: string;
-  lWidth?: string;
-  mdWidth?: string;
-  tabletWidth?: string;
-  tabletSmWidth?: string;
-  lgWidth?: string;
-  xlWidth?: string;
-  twoXlWidth?: string;
-  threeXlWidth?: string;
-  fourXlWidth?: string;
-  fiveXlWidth?: string;
-  sixXlWidth?: string;
-  xxsHeight?: string;
-  xsHeight?: string;
-  sHeight?: string;
-  mHeight?: string;
-  smHeight?: string;
-  lHeight?: string;
-  mdHeight?: string;
-  tabletHeight?: string;
-  tabletSmHeight?: string;
-  lgHeight?: string;
-  xlHeight?: string;
-  twoXlHeight?: string;
-  threeXlHeight?: string;
-  fourXlHeight?: string;
-  fiveXlHeight?: string;
-  sixXlHeight?: string;
+  responsive: Record<DeviceSize, ResponsiveProperties>;
 }
 
 export const HorizontalSeparator: React.FC<IHorizontalSeparatorProps> = ({
-  width,
-  height,
   background = "black",
-  xxsMargin,
-  xsMargin,
-  sMargin,
-  mMargin,
-  smMargin,
-  lMargin,
-  mdMargin,
-  tabletMargin,
-  tabletSmMargin,
-  lgMargin,
-  xlMargin,
-  twoXlMargin,
-  threeXlMargin,
-  fourXlMargin,
-  fiveXlMargin,
-  sixXlMargin,
-  margin,
-  xxsWidth,
-  xsWidth,
-  sWidth,
-  mWidth,
-  smWidth,
-  lWidth,
-  mdWidth,
-  tabletWidth,
-  tabletSmWidth,
-  lgWidth,
-  xlWidth,
-  twoXlWidth,
-  threeXlWidth,
-  fourXlWidth,
-  fiveXlWidth,
-  sixXlWidth,
-  xxsHeight,
-  xsHeight,
-  sHeight,
-  mHeight,
-  smHeight,
-  lHeight,
-  mdHeight,
-  tabletHeight,
-  tabletSmHeight,
-  lgHeight,
-  xlHeight,
-  twoXlHeight,
-  threeXlHeight,
-  fourXlHeight,
-  fiveXlHeight,
-  sixXlHeight,
+  responsive,
 }) => {
   const { width: windowWidth } = useWindowSize();
 
-  const getResponsiveValue = (
-    xxsValue: string | undefined,
-    xsValue: string | undefined,
-    sValue: string | undefined,
-    mValue: string | undefined,
-    smValue: string | undefined,
-    lValue: string | undefined,
-    mdValue: string | undefined,
-    tabletValue: string | undefined,
-    tabletSmValue: string | undefined,
-    lgValue: string | undefined,
-    xlValue: string | undefined,
-    twoXlValue: string | undefined,
-    threeXlValue: string | undefined,
-    fourXlValue: string | undefined,
-    fiveXlValue: string | undefined,
-    sixXlValue: string | undefined,
-    defaultValue: string | undefined
-  ) => {
-    const breakpoint = getBreakpoint(windowWidth);
-    switch (breakpoint) {
-      case "xxs":
-        return xxsValue || defaultValue;
-      case "xs":
-        return xsValue || defaultValue;
-      case "s":
-        return sValue || defaultValue;
-      case "m":
-        return mValue || defaultValue;
-      case "sm":
-        return smValue || defaultValue;
-      case "l":
-        return lValue || defaultValue;
-      case "md":
-        return mdValue || defaultValue;
-      case "tablet":
-        return tabletValue || defaultValue;
-      case "tablet_sm":
-        return tabletSmValue || defaultValue;
-      case "lg":
-        return lgValue || defaultValue;
-      case "xl":
-        return xlValue || defaultValue;
-      case "2xl":
-        return twoXlValue || defaultValue;
-      case "3xl":
-        return threeXlValue || defaultValue;
-      case "4xl":
-        return fourXlValue || defaultValue;
-      case "5xl":
-        return fiveXlValue || defaultValue;
-      case "6xl":
-        return sixXlValue || defaultValue;
-      default:
-        return defaultValue;
-    }
+  const getResponsiveProperty = (
+    property: keyof ResponsiveProperties,
+    defaultValue: string
+  ): string => {
+    const breakpoint: DeviceSize = getBreakpoint(windowWidth) as DeviceSize;
+    return responsive[breakpoint]?.[property] || defaultValue;
   };
 
-  const getWidth = () =>
-    getResponsiveValue(
-      xxsWidth,
-      xsWidth,
-      sWidth,
-      mWidth,
-      smWidth,
-      lWidth,
-      mdWidth,
-      tabletWidth,
-      tabletSmWidth,
-      lgWidth,
-      xlWidth,
-      twoXlWidth,
-      threeXlWidth,
-      fourXlWidth,
-      fiveXlWidth,
-      sixXlWidth,
-      width
-    );
+  const getMargin = () => getResponsiveProperty("margin", "5px");
 
-  const getHeight = () =>
-    getResponsiveValue(
-      xxsHeight,
-      xsHeight,
-      sHeight,
-      mHeight,
-      smHeight,
-      lHeight,
-      mdHeight,
-      tabletHeight,
-      tabletSmHeight,
-      lgHeight,
-      xlHeight,
-      twoXlHeight,
-      threeXlHeight,
-      fourXlHeight,
-      fiveXlHeight,
-      sixXlHeight,
-      height
-    );
+  const getWidth = () => getResponsiveProperty("width", "20px");
 
-  const getMargin = () =>
-    getResponsiveValue(
-      xxsMargin,
-      xsMargin,
-      sMargin,
-      mMargin,
-      smMargin,
-      lMargin,
-      mdMargin,
-      tabletMargin,
-      tabletSmMargin,
-      lgMargin,
-      xlMargin,
-      twoXlMargin,
-      threeXlMargin,
-      fourXlMargin,
-      fiveXlMargin,
-      sixXlMargin,
-      margin
-    );
+  const getHeight = () => getResponsiveProperty("width", "20px");
 
   return (
     <hr
